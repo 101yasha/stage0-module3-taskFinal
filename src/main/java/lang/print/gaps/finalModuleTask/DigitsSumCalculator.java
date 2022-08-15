a4ce09205aaef1b71cd532cb54b7329b55ac6860
dzmitry <zakharenka.dzmitry.1981@gmail.com>
2022-08-15 17:30:10 +0300

    stage0-module3-taskFinal

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,18 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        String str = Integer.toString(number);
+        //преобразовываем строку в массив
+        char[] myArray = str.toCharArray();
+        //перебираем все элементы массива
+        int sum = 0;
+        for (int i = 0; i < myArray.length; i++) {
+            int c = myArray[i];
+            int b = Character.digit(c, 10);
+            sum = sum + b;
+        }
+        number = sum;
+        System.out.println(number);
+        }
+
     }
-}
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,9 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        String str = Integer.toString(number);
+        StringBuilder newStr = new StringBuilder(str);
+        StringBuilder reverseStr = newStr.reverse();
+        System.out.println(reverseStr);
     }
 }
