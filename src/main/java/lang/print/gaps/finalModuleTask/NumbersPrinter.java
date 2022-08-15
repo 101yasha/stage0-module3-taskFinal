8294059999783713865504502e52f72e93ccb360
dzmitry <zakharenka.dzmitry.1981@gmail.com>
2022-08-15 18:00:26 +0300

    stage0-module3-taskFinal

--- src/main/java/lang/print/gaps/finalModuleTask/NumbersPrinter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumbersPrinter.java
@@ -2,10 +2,7 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumbersPrinter {
     public void printIsPositive(int number) {
-        if (number > 0 ) {
-            System.out.println(true);
-        }
-        System.out.println(false);
-    }
+        System.out.println((number) > 0);
+     }
 
 }
